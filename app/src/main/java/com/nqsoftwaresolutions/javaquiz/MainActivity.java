3330d98db1bea1f6965aae855979fb46efc12500
AbdulQayoomJat <abdulqayoomjat9@gmail.com>
2021-08-05 15:05:38 +0500

    Challenge Costomize Toast Completed..

--- app/src/main/java/com/nqsoftwaresolutions/javaquiz/MainActivity.java
+++ app/src/main/java/com/nqsoftwaresolutions/javaquiz/MainActivity.java
@@ -3,6 +3,7 @@ package com.nqsoftwaresolutions.javaquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -20,19 +21,26 @@ public class MainActivity extends AppCompatActivity {
         mFalseButton = findViewById(R.id.id_btn_false);
         mTrueButton = findViewById(R.id.id_btn_true);
 
+        /**
+         * Todo Challenge customize Toast
+         * show Toast on top of the screen
+         */
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String ans = getString(R.string.notCorrect);
-                Toast.makeText(MainActivity.this, ans, Toast.LENGTH_SHORT).show();
+                Toast toast = Toast.makeText(MainActivity.this, R.string.notCorrect, Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP,1,80);
+                toast.show();
             }
         });
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String ans = getString(R.string.correct);
-                Toast.makeText(MainActivity.this, ans, Toast.LENGTH_SHORT).show();
+                Toast toast = Toast.makeText(MainActivity.this, R.string.correct, Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP,1,80);
+                toast.show();
             }
         });
+
     }
 }
