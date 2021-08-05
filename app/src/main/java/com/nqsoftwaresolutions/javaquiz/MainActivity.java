a9bf5339aab14bdfc1c87d5fd7d89cd04ebaef74
AbdulQayoomJat <abdulqayoomjat9@gmail.com>
2021-08-05 12:30:38 +0500

    Java Activty is complete commit for version1

--- app/src/main/java/com/nqsoftwaresolutions/javaquiz/MainActivity.java
+++ app/src/main/java/com/nqsoftwaresolutions/javaquiz/MainActivity.java
@@ -3,12 +3,36 @@ package com.nqsoftwaresolutions.javaquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mFalseButton, mTrueButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFalseButton = findViewById(R.id.id_btn_false);
+        mTrueButton = findViewById(R.id.id_btn_true);
+
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String ans = getString(R.string.notCorrect);
+                Toast.makeText(MainActivity.this, ans, Toast.LENGTH_SHORT).show();
+            }
+        });
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String ans = getString(R.string.correct);
+                Toast.makeText(MainActivity.this, ans, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
