e303dd831c086a71404d8c983c6c3f45d1886a4a
AbdulQayoomJat <abdulqayoomjat9@gmail.com>
2021-08-06 08:32:53 +0500

    Question one from array is set...

--- app/src/main/java/com/nqsoftwaresolutions/javaquiz/MainActivity.java
+++ app/src/main/java/com/nqsoftwaresolutions/javaquiz/MainActivity.java
@@ -6,12 +6,27 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.nqsoftwaresolutions.javaquiz.DataModel.Questions;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button mFalseButton, mTrueButton;
+    private Button mFalseButton, mTrueButton, mNextButton;
+    private TextView mQuestionTextView;
+    /**
+     * Create a question array
+     */
+    private Questions[] mQuestionsBank = new Questions[]{
+            new Questions(R.string.java_fundamentals,false),
+            new Questions(R.string.java_fundamentals2,true),
+            new Questions(R.string.java_fundamentals3,false),
+            new Questions(R.string.java_fundamentals4,false),
+            new Questions(R.string.java_fundamentals5,true)
+    };
+    private int mCurrentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +35,12 @@ public class MainActivity extends AppCompatActivity {
 
         mFalseButton = findViewById(R.id.id_btn_false);
         mTrueButton = findViewById(R.id.id_btn_true);
+        mNextButton = findViewById(R.id.id_btn_next);
+
+        //Todo Get reference of Question text view & assign it text on current index
+        mQuestionTextView = findViewById(R.id.id_txt_question);
+        int question = mQuestionsBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
 
         /**
          * Todo Challenge customize Toast
@@ -39,6 +60,13 @@ public class MainActivity extends AppCompatActivity {
                 Toast toast = Toast.makeText(MainActivity.this, R.string.correct, Toast.LENGTH_SHORT);
                 toast.setGravity(Gravity.TOP,1,80);
                 toast.show();
+            }
+        });
+
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
         });
 
