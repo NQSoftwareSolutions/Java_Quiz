5d27c6db5dd5381b9095afead5931e1baf3aa0f8
AbdulQayoomJat <abdulqayoomjat9@gmail.com>
2021-08-14 11:10:01 +0500

    problem 1 saving stat of CheatActivity is solved

--- app/src/main/java/com/nqsoftwaresolutions/javaquiz/CheatActivity.java
+++ app/src/main/java/com/nqsoftwaresolutions/javaquiz/CheatActivity.java
@@ -2,10 +2,12 @@ package com.nqsoftwaresolutions.javaquiz;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CheatActivity extends AppCompatActivity {
@@ -13,24 +15,34 @@ public class CheatActivity extends AppCompatActivity {
     private static final String EXTRA = "answer";
     private static final String RESULT_EXTRA = "Cheating";
     private TextView mShowAnswerTv;
+    private static final String IS_CHEATED_KEY = "Cheat";
+    private static final String TAG = "CheatActivity";
+
+    private boolean isCheated;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG,"Cheat activity Created");
         setContentView(R.layout.activity_cheat);
+
+        checkLastActivityState(savedInstanceState);
+        Log.d(TAG,"Current Value of isCheated = "+isCheated);
 
         mShowAnswerTv = findViewById(R.id.id_txt_show_answer);
     }
 
     /**Todo set data for parent activity that user cheated or not
      * @param isCheated It will be true it user cheat else false
+     * @return isCheated b/c we want to store that
      * This method will create an Intent & Send data that user has Cheated  or Not
      * we are using this method on Show Answer button so it mean user has cheated
      */
-    private void setAnswerShownResult(boolean isCheated) {
+    private boolean setAnswerShownResult(boolean isCheated) {
         Intent data = new Intent();
         data.putExtra(RESULT_EXTRA, isCheated);
         setResult(RESULT_OK, data);
+        return isCheated;
     }
 
     /**Todo get the result from received intent
@@ -55,6 +67,28 @@ public class CheatActivity extends AppCompatActivity {
         }else {
             mShowAnswerTv.setText(R.string.aFalse);
         }
-        setAnswerShownResult(true);
+        isCheated = setAnswerShownResult(true);
+    }
+
+    /**Todo Save the state of activity to get last value of isCheated
+     * @param outState state of activity
+     *                 Todo Challenge 5.1 Closing loop holes & save state
+     */
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(IS_CHEATED_KEY,isCheated);
+    }
+
+    /**Todo Check the last state of activity
+     * @param savedInstanceState bundle of last activity data
+     *                           get the value of isCheated if its is saved in bundle
+     *                           & assign that value to isCheated variable
+     *                           Todo Challenge 5.1 Closing loop holes & save state
+     */
+    private void checkLastActivityState(Bundle savedInstanceState) {
+        if (savedInstanceState != null){
+            isCheated = savedInstanceState.getBoolean(IS_CHEATED_KEY);
+        }
     }
 }
